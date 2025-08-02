09d5f0a211ded2232750aa3458b7ab239b321dad
bliepp <12013202+bliepp@users.noreply.github.com>
2025-08-03 00:38:14 +0200

    Set default values

--- src/index.js
+++ src/index.js
@@ -2,10 +2,32 @@ import "./index.scss";
 
 
 window.addEventListener("load", () => {
-	const day = document.querySelector("form input#day");
-	const month = document.querySelector("form input#month");
-	const year = document.querySelector("form input#year");
+	// get DOM elements
+	const form = document.querySelector("form");
+	const day = form.querySelector("form input#day");
+	const month = form.querySelector("form input#month");
+	const year = form.querySelector("form input#year");
 
-	const submit = document.querySelector("form button[type=submit");
-	const output = document.querySelector("form input:read-only");
+	const submit = form.querySelector("button[type=submit");
+	const output = form.querySelectorAll("input:read-only");
+
+	// set default values
+	let now = new Date();
+	day.placeholder = ("0" + now.getDate().toString()).slice(-2);
+	month.placeholder = ("0" + (now.getMonth() + 1).toString()).slice(-2);
+	year.placeholder = now.getFullYear().toString();
+
+	// do something on form submit
+	form.addEventListener("submit", (e) => {
+		e.preventDefault();
+
+		let dateString = `
+            ${year.value || year.placeholder}
+            -${month.value || month.placeholder}
+            -${day.value || day.placeholder}
+        `.split("\n").map(s => s.trim()).join("");
+		let date = Date.parse(dateString);
+
+		console.log(date)
+	});
 });
