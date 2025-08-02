89fbae0fc350f78f88476ce09c14e5306c8dd845
bliepp <12013202+bliepp@users.noreply.github.com>
2025-08-03 00:28:30 +0200

    Scaffold vite project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./index.scss";
+
+
+window.addEventListener("load", () => {
+	const day = document.querySelector("form input#day");
+	const month = document.querySelector("form input#month");
+	const year = document.querySelector("form input#year");
+
+	const submit = document.querySelector("form button[type=submit");
+	const output = document.querySelector("form input:read-only");
+});
